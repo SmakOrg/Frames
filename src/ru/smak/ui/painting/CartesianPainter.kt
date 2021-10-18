4889d28b2b713983425319b1434190c28816dc2d
Sergey Makletsov <smaklets@kpfu.ru>
2021-10-18 12:51:26 +0300

    Плоскость, Пэйнтер

--- src/ru/smak/ui/painting/CartesianPainter.kt
+++ src/ru/smak/ui/painting/CartesianPainter.kt
@@ -1,15 +1,24 @@
 package ru.smak.ui.painting
 
+import java.awt.BasicStroke
 import java.awt.Color
 import java.awt.Graphics
+import java.awt.Graphics2D
 
 class CartesianPainter(private val plane: CartesianPlane) {
 
-    fun paint(g: Graphics){
+    fun paint(g: Graphics) {
         with(plane) {
-            g.apply {
-                drawLine(0, yCrt2Scr(0.0), width, yCrt2Scr(0.0))
-                drawLine(xCrt2Scr(0.0), 0, xCrt2Scr(0.0), height)
+            (g as Graphics2D).apply {
+                stroke = BasicStroke(3F)
+                if ((xMin > 0) or (xMax < 0)) {
+                    drawLine(width, 0, width, height)
+                    drawLine(0, 0, 0, height)
+                } else drawLine(xCrt2Scr(0.0), 0, xCrt2Scr(0.0), height)
+                if ((yMin > 0) or (yMax < 0)) {
+                    drawLine(0, height, width, height)
+                    drawLine(0, 0, width, 0)
+                } else drawLine(0, yCrt2Scr(0.0), width, yCrt2Scr(0.0))
             }
         }
     }
